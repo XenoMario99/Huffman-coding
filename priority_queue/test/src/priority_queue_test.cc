4b580202cef8b2d294dbe7da8e76476855e45d55
Jakub Kozdra <jacobs12@interia.pl>
2023-12-07 21:28:55 +0100

    Add some tests

--- priority_queue/test/src/priority_queue_test.cc
+++ priority_queue/test/src/priority_queue_test.cc
@@ -2,30 +2,40 @@
 
 #include <gtest/gtest.h>
 
+#include <queue>
+
 namespace queue {
 
 class PriorityQueueTest : public ::testing::Test {
    protected:
     PriorityQueue<int> queue;
+    std::priority_queue<int> refQueue;
+    std::vector<int> arr{12, 11, 13, 5, 6, 7, 19, 1, 41, 3, 8, 7};
+    int size;
 
-    void SetUp() override{};
+    void SetUp() override {
+        for (int i = 0; i < arr.size(); i++) {
+            refQueue.push(arr[i]);
+        }
+        size = arr.size();
+    };
     void TearDown() override{};
 
     bool checkMaxHeapProperty() const {
         auto refQueue{queue};
         while (!refQueue.empty()) {
-            const auto& top{refQueue.top()};
+            const auto top{refQueue.top()};
             refQueue.pop();
 
             if (!refQueue.empty()) {
-                const auto& leftLeaf{refQueue.top()};
+                const auto leftLeaf{refQueue.top()};
                 refQueue.pop();
 
                 if (top < leftLeaf) return false;
             }
 
             if (!refQueue.empty()) {
-                const auto& rightLeaf{refQueue.top()};
+                const auto rightLeaf{refQueue.top()};
                 refQueue.pop();
 
                 if (top < rightLeaf) return false;
@@ -36,10 +46,33 @@ class PriorityQueueTest : public ::testing::Test {
     }
 };
 
-TEST_F(PriorityQueueTest, test1) {
-    std::vector<int> arr{12, 11, 13, 5, 6, 7, 19, 1, 41, 3, 8, 7};
+TEST_F(PriorityQueueTest, buildHeap_ok) {
     queue.buildHeap(arr);
     EXPECT_TRUE(checkMaxHeapProperty());
+}
+
+TEST_F(PriorityQueueTest, pop_ok) {
+    queue.buildHeap(arr);
+    const auto size{queue.size()};
+    const int popNum{3};
+    for (int i = 0; i < popNum; i++) {
+        queue.pop();
+    }
+    EXPECT_TRUE(checkMaxHeapProperty());
+    EXPECT_EQ(size, queue.size() + popNum);
+}
+
+TEST_F(PriorityQueueTest, top_and_pop_ok) {
+    queue.buildHeap(arr);
+    for (int i = 0; i < size; i++) {
+        const auto queueTop{queue.top()};
+        const auto refQueueTop{refQueue.top()};
+        EXPECT_EQ(queueTop, refQueueTop);
+        queue.pop();
+        refQueue.pop();
+    }
+
+    EXPECT_EQ(queue.size(), refQueue.size());
 }
 
 }  // namespace queue
